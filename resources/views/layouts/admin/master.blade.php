18f6d829c8d32bcff38088b04da286a91dd8ac84
RafiAfdhal <afdalrafi46@gmail.com>
2025-10-08 10:54:45 +0800

    latest update

--- resources/views/layouts/admin/master.blade.php
+++ resources/views/layouts/admin/master.blade.php
@@ -23,25 +23,42 @@
             <div class="d-flex align-items-center gap-3">
                 {{-- Notifikasi Pesan --}}
                 @php
-                use App\Models\PesanKontak;
-                $unreadCount = PesanKontak::where('is_read', false)->count();
+                    use App\Models\PesanKontak;
+                    $unreadCount = PesanKontak::where('is_read', false)->count();
                 @endphp
 
                 <a href="{{ route('admin.pesan.index') }}" class="nav-link position-relative">
                     <i class="fas fa-envelope"></i>
-                    @if($unreadCount > 0)
-                    <span class="badge bg-info position-absolute top-0 start-100 translate-middle">
-                        {{ $unreadCount }}
-                    </span>
+                    @if ($unreadCount > 0)
+                        <span class="badge bg-info position-absolute top-0 start-100 translate-middle">
+                            {{ $unreadCount }}
+                        </span>
                     @endif
                 </a>
                 <ul class="navbar-nav">
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle d-flex align-items-center" href="#" id="navbarDropdown"
                             role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            <img src="{{ auth()->user()->profile_photo_url ?? asset('images/default-profile.png') }}"
-                                alt="Foto Profil" class="rounded-circle me-2" width="32" height="32">
-                            <span>{{ auth()->user()->name }}</span>
+                            @php
+                                $user = auth()->user();
+                                $hasPhoto =
+                                    !empty($user->profile_photo_url) &&
+                                    file_exists(public_path($user->profile_photo_url));
+                                $firstLetter = strtoupper(substr($user->name, 0, 1));
+                            @endphp
+
+                            @if ($hasPhoto)
+                                <img src="{{ asset($user->profile_photo_url) }}" alt="Foto Profil"
+                                    class="rounded-circle me-2" width="32" height="32">
+                            @else
+                                <div class="rounded-circle bg-warning text-dark d-flex justify-content-center align-items-center me-2"
+                                    style="width: 32px; height: 32px; font-weight: bold;">
+                                    {{ $firstLetter }}
+                                </div>
+                            @endif
+
+                            <span>{{ $user->name }}</span>
+
                         </a>
                         <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                             <li>
